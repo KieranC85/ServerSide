243d4ae9b8a5877f40d60405efd04e20bb4e301c
EC2 Default User <ec2-user@ip-172-31-47-131.ec2.internal>
2020-04-09 19:40:53 +0000

    updated to tutorial 8

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,4 +10,19 @@ class StaticPagesController < ApplicationController
 
   def products
   end
+  
+  def createOrder
+  
+  # @user= User.find(current_user.id)
+  
+   # may need more here see Tutorial 8
+  
+  end 
+  
+  @orders = Order.last
+  
+  @orderitems = Orderitem.where(order_id: Order.last)
+  
+  session[:cart] = nil
+  
 end
