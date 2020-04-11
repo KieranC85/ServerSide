5d54fc836ba7b668f8aa4f0ccdf3adc0bb46dc3b
Donald Hickey <56938143+donaldhickey@users.noreply.github.com>
2020-04-11 12:27:47 +0100

    Update static_pages_controller.rb

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -34,14 +34,13 @@ def paid
     
   end
 
-
   def paid
     # redirect_to "/cart/clear"
-  
-    
+
     @order = Order.find_by(id: params[:id])
     @order.update_attribute(:status , "Paid by User: #{current_user.email}")
     #"Paid by User:#{current_user.id} #{current_user.name} #{current_user.surname}")
     
   end
+
 end
