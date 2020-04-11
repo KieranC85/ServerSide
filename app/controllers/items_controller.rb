f8f7dcf1a7e0ab7a22a1b10a142367cce6c4b548
Donald Hickey <56938143+donaldhickey@users.noreply.github.com>
2020-04-11 13:16:10 +0100

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,23 @@ class ItemsController < ApplicationController
 
   # GET /items/1
   # GET /items/1.json
+ 
   def show
-    
+    # to show order items
     @orderitems = Orderitem.all
     @orderitems = Orderitem.where(order_id: params[:id])
+    
+    #Orders Per Customer - To filter orders by user logged iin
+    @user = User.find(current_user.id)
+    @orders = @user.orders.all
+    
+    <% if @order.user.email == @user.email %>
+    # to stop the wrong person seeing the wrong order
+    @user = User.find(current_user.id)
+    <% else %>
+    <h1>You did not place this order</h1>
+    <% end %>
+    
 
   end
 
