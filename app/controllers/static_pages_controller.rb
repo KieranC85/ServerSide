4b9f756ccf9c4333b22588926fed996385e03a27
EC2 Default User <ec2-user@ip-172-31-47-131.ec2.internal>
2020-04-11 14:05:21 +0000

    small fixes to tut 8

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,24 +11,27 @@ class StaticPagesController < ApplicationController
 
   def category
   catName = params[:title]
-    @items = Item.where("category like?, catName)
+    @items = Item.where("category like?", catName)
   end
   
   def createOrder
-  
-  # @user= User.find(current_user.id)
-  
-   # may need more here see Tutorial 8
-  
-  end 
-  
-  @orders = Order.last
-  
-  @orderitems = Orderitem.where(order_id: Order.last)
-  
-  session[:cart] = nil
-  
-def paid
+     # Step 1: Get the current user
+     @user = User.find(current_user.id)
+    
+     # Step 2: Create a new order and associate it with the current user
+     @order = @user.orders.build(:order_date => DateTime.now, :status => 'Pending')
+     @order.save
+    
+     # Step 3: For each item in the cart, create a new item on the order!!
+     @cart = session[:cart] || {} # Get the content of the Cart
+     @cart.each do | id, quantity |
+     item = Item.find_by_id(id)
+     @orderitem = @order.orderitems.build(:item_id => item.id, :title => item.title, :description => item.description, :quantity => quantity, :price=> item.price)
+     @orderitem.save
+  end
+
+
+  def paid
     # redirect_to "/cart/clear"
     flash[:notice] = 'Transaction Complete'
     @order = Order.last
@@ -39,11 +42,11 @@ def paid
 
   def paid
     # redirect_to "/cart/clear"
-
+    flash[:notice] = 'Transaction Complete'
     @order = Order.find_by(id: params[:id])
     @order.update_attribute(:status , "Paid by User: #{current_user.email}")
     #"Paid by User:#{current_user.id} #{current_user.name} #{current_user.surname}")
     
   end
 
-end
+#end
